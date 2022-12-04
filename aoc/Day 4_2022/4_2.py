371595219f88646b4006b1251112ea35c037b630
Justine Liene Laumane <justinellaumane@gmail.com>
2022-12-04 11:44:33 +0200

    Day 4 2022

--- aoc/Day 4_2022/4_2.py
+++ aoc/Day 4_2022/4_2.py
@@ -1,16 +1,34 @@
 from pyaoc import aoc
 import re
 
-@aoc(2022, 4, 2) 
-def two(data=None): #faster
-    count = 0
-    for i in data.splitlines():
-        listy = list(map(int,re.findall(r'\d+', i)))
-        if listy[0]<=listy[2] and listy[1]>=listy[2]:
-            count+=1
-        elif listy[0]>=listy[2] and listy[0]<=listy[3]:
-            count+=1
-    return (count)
+@aoc(2022, 4, 2)
+def two(data=None):  #fastest
+  count = 0
+  for i in data.splitlines():
+    listy = list(map(int, re.findall(r'\d+', i)))
+    if listy[0] <= listy[2] and listy[1] >= listy[2] or listy[0] >= listy[2] and listy[0] <= listy[3]:
+      count += 1
+  return count
+
+# @aoc(2022, 4, 2)
+# def two(data=None):  #readable
+#   count = 0
+#   for i in data.splitlines():
+#     a, b, c, d = list(map(int, re.findall(r'\d+', i)))
+#     if a <= c and b >= c or a >= c and a <= d:
+#       count += 1
+#   return count
+
+# @aoc(2022, 4, 2) 
+# def two(data=None): #faster (ish)
+#     count = 0
+#     for i in data.splitlines():
+#         listy = list(map(int,re.findall(r'\d+', i)))
+#         if listy[0]<=listy[2] and listy[1]>=listy[2]:
+#             count+=1
+#         elif listy[0]>=listy[2] and listy[0]<=listy[3]:
+#             count+=1
+#     return (count)
 
 # @aoc(2022, 4, 2)
 # def two(data=None):
