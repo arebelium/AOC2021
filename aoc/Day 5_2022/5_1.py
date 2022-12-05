e1101b83c4121b7a16748cdcdbbbdc55eb3de0a5
Justine Liene Laumane <justinellaumane@gmail.com>
2022-12-05 21:36:49 +0200

    Day 5 2022

--- aoc/Day 5_2022/5_1.py
+++ aoc/Day 5_2022/5_1.py
@@ -4,16 +4,14 @@ import re
 @aoc(2022, 5, 1)
 def one(data=None):
     total = []
+    box = []
+    for i in (list(data.split("\n\n")[0].splitlines())):
+        box.append([i[1], i[5], i[9], i[13], i[17], i[21], i[25], i[29], i[33]])
+    for i in zip(*box[::-1]):
+        i = [value for value in i if value != " "]
+        i.pop(0)
+        total.append(i)
     answer = []
-    total.append(['D', 'L', 'V', 'T', 'M', 'H', 'F'])
-    total.append(['H', 'Q', 'G', 'J', 'C', 'T', 'N', 'P'])
-    total.append(['R', 'S', 'D', 'M', 'P', 'H'])
-    total.append(['L', 'B', 'V', 'F'])
-    total.append(['N', 'H', 'G', 'L', 'Q'])
-    total.append(['W', 'B', 'D', 'G', 'R', 'M', 'P'])
-    total.append(['G', 'M', 'N', 'R', 'C', 'H', 'L', 'Q'])
-    total.append(['C', 'L', 'W'])
-    total.append(['R', 'D', 'L', 'Q', 'J', 'Z', 'M', 'T'])
     for i in data.split("\n\n")[1].splitlines():
         listy = list(map(int,re.findall(r'\d+', i)))
         for j in range(0, listy[0]):
