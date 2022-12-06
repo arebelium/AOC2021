7b8c767fd740950d9d5f155765055401d48491e1
Justine Liene Laumane <justinellaumane@gmail.com>
2022-12-06 16:22:16 +0200

    Day 6 2022

--- aoc/Day 6_2022/6_2.py
+++ aoc/Day 6_2022/6_2.py
@@ -1,7 +1,7 @@
 from pyaoc import aoc
 
 @aoc(2022, 6, 2)
-def one(data=None):
+def two(data=None):
     for i in range(14, len(data)):
         if len(set(data[i:i+14:1])) == 14:
             return(i+14)
