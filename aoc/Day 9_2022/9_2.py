dc057b8692fd0b3c8a2c852080c8e263b3fa7ae4
Justine Liene Laumane <justinellaumane@gmail.com>
2022-12-12 20:25:37 +0200

    Day 10

---
+++ aoc/Day 9_2022/9_2.py
@@ -0,0 +1,67 @@
+from pyaoc import aoc
+
+@aoc(2022, 9, 2)
+def two(data=None):
+    # data="R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
+    HrowI = 0
+    HcolI = 0
+    TrowI = 0
+    TcolI = 0
+    all={}
+    all[str(TrowI)+":"+str(TcolI)] = 1
+    for i in data.splitlines():
+        for index in range(1, int(i.split(" ")[1])+1):
+            match i.split(" ")[0]:
+                case "R":
+                    if HrowI==TrowI and HcolI==TcolI+1:
+                        TcolI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI+1==TrowI and HcolI==TcolI+1:
+                        TcolI+=1
+                        TrowI-=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI==TrowI+1 and HcolI==TcolI+1:
+                        TcolI+=1
+                        TrowI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    HcolI+=1
+                case "L":
+                    if HrowI+1==TrowI and HcolI+1==TcolI:
+                        TcolI-=1
+                        TrowI-=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI==TrowI+1 and HcolI+1==TcolI:
+                        TcolI-=1
+                        TrowI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI==TrowI and HcolI+1==TcolI:
+                        TcolI-=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    HcolI-=1
+                case "U":
+                    if HrowI==TrowI+1 and HcolI==TcolI+1:
+                        TcolI+=1
+                        TrowI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI==TrowI+1 and HcolI+1==TcolI:
+                        TcolI-=1
+                        TrowI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI==TrowI+1 and HcolI==TcolI:
+                        TrowI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    HrowI+=1
+                case "D":
+                    if HrowI+1==TrowI and HcolI==TcolI:
+                        TrowI-=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI+1==TrowI and HcolI==TcolI+1:
+                        TrowI-=1
+                        TcolI+=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    if HrowI+1==TrowI and HcolI+1==TcolI:
+                        TrowI-=1
+                        TcolI-=1
+                        all[str(TrowI)+":"+str(TcolI)] = 1
+                    HrowI-=1
+    print(len(all))
